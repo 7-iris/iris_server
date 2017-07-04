9764cd0e059bce5aa8b903856509a88ba9d487c1
jkarr <jkarr@amigdalo.tk>
2017-07-04 23:44:03 +0300

    Add indicators for success and error messages

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,11 @@
 defmodule Iris.PageController do
   use Iris.Web, :controller
 
-  def index(conn, _params) do
+  def index(conn, _) do
     render conn, "index.html"
+  end
+
+  def dashboard(conn, _) do
+    render conn, "dashboard.html"
   end
 end
